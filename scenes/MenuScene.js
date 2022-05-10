81f022e4843e874968c0d4930c316c877772139a
Sam Feng <fengyuan20000303@gmail.com>
2022-05-10 09:08:23 -0700

    Update MenuScene.js

--- scenes/MenuScene.js
+++ scenes/MenuScene.js
@@ -5,7 +5,7 @@ class MenuScene extends Phaser.Scene {
 
     preload() {
         // load images
-        this.load.image("bg", "./assets/bg.png");
+        this.load.image("bg", "./assets/bg.jpg");
     
     }
 
@@ -13,14 +13,25 @@ class MenuScene extends Phaser.Scene {
         // add floor
         this.add.rectangle(0, 0, 2000, 1000, 0x000000);
 
+        // add background
+        this.bg = this.add.image(game.config.width / 2, game.config.height / 2, "bg");
+        this.bg.setOrigin(0.5, 0.5);
+        this.bg.setScale(2);
+
         // add title
         this.title = this.add.text((game.config.width / 2), (game.config.height / 4) - 50, ["Roly Poly", "To the End"]);
         this.title.setOrigin(0.5, 0.5);
         this.title.setFontSize(94);
+        
+        // click to start
+        this.input.on("pointerdown", () => {
+            this.scene.start("MainGameScene");
+        });
+
     }
 
     update() {
-
+        
     }
 
 }
