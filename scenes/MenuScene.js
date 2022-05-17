0a22c19065e1697b0bb216686ea2c6886b23c6bc
Sam Feng <fengyuan20000303@gmail.com>
2022-05-17 08:42:44 -0700

    Update MenuScene.js

--- scenes/MenuScene.js
+++ scenes/MenuScene.js
@@ -37,6 +37,28 @@ class MenuScene extends Phaser.Scene {
             repeat: -1
         });
 
+        // add instructions
+        this.instructions = this.add.text((game.config.width / 2), (game.config.height / 2) + 200, ["Click to Start", "Press ESC to Quit"], {fontFamily: "Butter", fontSize: 32, color: "#000000"});
+        this.instructions.setOrigin(0.5, 0.5);
+        this.instructions.setFontSize(50);
+        // this.instructions.addFontShadow({color: "#ffffff"});
+        this.tweens.add({
+            targets: this.instructions,
+            scale: {
+                from: 1,
+                to: 1.1,
+            },
+            rotation: {
+                from: -0.1,
+                to: 0.1,
+            },
+            duration: 2000,
+            ease: "Bounce",
+            yoyo: true,
+            repeat: -1
+        });
+
+
         // add slug
         this.slug = this.add.image(game.config.width / 2, game.config.height - 100, "slug");
         this.slug.setOrigin(0.5, 0.5);
@@ -89,8 +111,8 @@ class MenuScene extends Phaser.Scene {
         this.tweens.add({
             targets: this.roly,
             x: {
-                from: game.config.width,
-                to: 0,
+                from: 0,
+                to: game.config.width,
             },
             duration: 5000,
             repeat: -1
