f1eab9354df4a21f9d5a68b39c0844dfeefb1c67
Denae <dfickes@ucsc.edu>
2022-05-17 09:01:41 -0700

    Some notes

--- scenes/MainGameScene.js
+++ scenes/MainGameScene.js
@@ -4,6 +4,7 @@ class MainGameScene extends Phaser.Scene {
     }
 
     preload() {
+        //load images
         this.load.image('player', 'assets/roly poly final art.png');
         this.load.image('slug', 'assets/slug.png');
         this.load.image('ground', 'assets/ground.png');
@@ -11,27 +12,29 @@ class MainGameScene extends Phaser.Scene {
     }
 
     create() {
+        //create basics
         this.cameras.main.setBackgroundColor(0x9edffa);
         this.bugScale = 0.5;
         this.slug_spawn_interval = 3000;
         this.game_speed = 10;
-
+        //add pointer
         this.pointer = this.input.activePointer;
 
+        //add ground
         //this.player = this.add.sprite(200, game.config.height/2, 'player').setScale(this.bugScale);
         this.ground = this.physics.add.sprite(game.config.width/2, game.config.height*0.8, 'ground');
         this.ground.setOrigin(0.5, 0);
-
+        //add grass
         this.grass1 = this.add.tileSprite(game.config.width/2, game.config.height/2, game.config.width, game.config.height/3, 'grass');
         this.grass2 = this.add.tileSprite(game.config.width/2, game.config.height/2-100, game.config.width, game.config.height/3, 'grass').setScale(1.2).setTint(0x989898).setDepth(-1);
         this.ground.displayWidth = game.config.width * 1.1;
         this.ground.displayHeight = game.config.height * 0.4;
-
+        //add ground to not move
         this.ground.setImmovable();
-
+        //add player
         this.player = new Player(this, game.config.width * 0.3, game.config.height * 0.8, 'player', this.bugScale);
         this.obstacles = [];
-
+        //add gravity
         this.physics.add.collider(this.player, this.ground);
         this.physics.add.overlap(this.player, this.obstacles, function(player, slug) {
             console.log("OUCH!");
